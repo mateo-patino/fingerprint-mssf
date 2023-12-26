8a9fcfbff8d38646f91407266a5a77b338fe52aa
Mateo Patiño Hasbon <mateopatinohasbon@Mateos-MacBook-Air.local>
2023-12-26 16:03:29 -0500

    Added helpful comments.

--- testing.py
+++ testing.py
@@ -1,2 +1,3 @@
-print("Yes, git works.")
+#   This file (and testing1.py) are used for testing code chunks on the side without disrupting
+#   disrupting the major files (analyze.py, cross.py, etc.).
     
