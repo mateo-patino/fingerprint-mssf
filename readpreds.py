73c35a52d5444792d8dd81d1beb19003ab904ef7
Mateo Patiño Hasbon <mateopatinohasbon@Mateos-MacBook-Air.local>
2024-02-10 14:36:04 -0500

    Modified log files for stats on word experiments

--- readpreds.py
+++ readpreds.py
@@ -4,11 +4,15 @@ from scipy.stats import ttest_1samp, sem
 
 prefix = "/Users/mateopatinohasbon/Documents/bigger-fish-main/fingerprint-mssf/chrome_traces/training/predictions/words/"
 paths = [
+    f"{prefix}200-100words.npz",
+    f"{prefix}300-100words.npz",
     f"{prefix}400-100words.npz",
+    f"{prefix}500-100words.npz",
     f"{prefix}10k-100words.npz",
     f"{prefix}40k-100words.npz",
     f"{prefix}100k-100words.npz",
     f"{prefix}200k-100words.npz",
+    f"{prefix}400k-100words.npz",
 ]
 for path in paths:
     data = np.load(path)
@@ -22,8 +26,11 @@ for path in paths:
         f1_scores.append(f1_score(true[i], preds[i].argmax(axis=1)) * 100)
         top1_scores.append(top_k_accuracy_score(true[i], preds[i][:, 1], k=1) * 100)
 
-    browser = "CHROME"
+    browser = "FIREFOX"
     title = f"{browser} - F1 AND TOP-1 SCORES FOR {path.split('/')[-1]}"
+    print()
+    print("----------------------------------------------------------")
+    print()
     print(title)
     print(f"F1 mean: {round(np.mean(f1_scores), 2)} ± {round(sem(f1_scores), 1)}")
     print(f"Top-1 mean: {round(np.mean(top1_scores), 2)} ± {round(sem(top1_scores), 1)}")
